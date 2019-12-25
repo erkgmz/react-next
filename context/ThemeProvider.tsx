bb821a1729fa78e8faef9e765a597eb8700c6507
Erik Gomez <e@erikgomez.co>
2019-12-24 18:03:29 -0800

    add ThemeContext and ThemeContextProvider

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+export const ThemeContext = React.createContext({});
+
+export const ThemeContextProvider = ({ children }) => {
+  const [currentHour, setCurrentHour] = useState(new Date().getHours());
+  const isLightRange = currentHour >= 7 && currentHour <= 18;
+  const [theme, setTheme] = useState(isLightRange ? 'light' : 'dark');
+
+  useEffect(() => {
+    // interval watch for hour change
+    const intervalId = setInterval(() => {
+      if (currentHour !== new Date().getHours()) {
+        setCurrentHour(new Date().getHours());
+        (setTheme as Function)(isLightRange ? 'light' : 'dark');
+      }
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [currentHour, isLightRange]);
+
+  return (
+    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+  );
+};
