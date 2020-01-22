9d94680102583dab6b27ac0bccbd80e250e91367
Erik Gomez <e@erikgomez.co>
2020-01-21 19:46:57 -0800

    Remove hardcoded theme value

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -5,16 +5,14 @@ export const ThemeContext = React.createContext([{}, () => {}]);
 export const ThemeContextProvider = ({ children }) => {
   const [currentHour, setCurrentHour] = useState(new Date().getHours());
   const isLightRange = currentHour >= 7 && currentHour <= 18;
-  // const [theme, setTheme] = useState(isLightRange ? 'light' : 'dark');
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(isLightRange ? 'light' : 'dark');
 
   useEffect(() => {
     // interval watch for hour change
     const intervalId = setInterval(() => {
       if (currentHour !== new Date().getHours()) {
         setCurrentHour(new Date().getHours());
-        // (setTheme as Function)(isLightRange ? 'light' : 'dark');
-        (setTheme as Function)('light');
+        (setTheme as Function)(isLightRange ? 'light' : 'dark');
       }
     }, 5000);
     return () => clearInterval(intervalId);
